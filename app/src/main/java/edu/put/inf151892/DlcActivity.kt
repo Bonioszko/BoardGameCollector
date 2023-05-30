cc09a13f60c230e94d59461fda1eef3f6f3fcd83
Bartek Pers <bartekpers@Barteks-MacBook-Air.local>
2023-05-30 10:06:55 +0200

    added extension list

--- app/src/main/java/edu/put/inf151892/DlcActivity.kt
+++ app/src/main/java/edu/put/inf151892/DlcActivity.kt
@@ -2,10 +2,25 @@ package edu.put.inf151892
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import models.Boardgame
+import models.Boardgame_RecylerViewAdapter
 
 class DlcActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dlc)
+
+        val listGames: MutableList<Boardgame> = mutableListOf()
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_list_games)
+            val db = DBHandler(this)
+            var listOfExtensions = db.getAllExtensions()
+            //val url ="https://boardgamegeek.com/xmlapi2/collection?username=rahdo"
+            //var text = XmlParserTask().execute(url)
+            val recyclerview = findViewById<RecyclerView>(R.id.RecyclerViewGames)
+            recyclerview.layoutManager = LinearLayoutManager(this)
+            val adapter = Boardgame_RecylerViewAdapter(listOfExtensions)
+            recyclerview.adapter = adapter
+
     }
 }
